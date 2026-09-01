11928e38f2f1a9f0c76349da6fe0c40b739106b9
ojasvichaurasia-777 <OJASVICHAURASIA.33520@stu.upes.ac.in>
2026-09-01 22:55:23 +0530

    Add files via upload

---
+++ day23.c
@@ -0,0 +1,39 @@
+// Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms.
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0, numerator, denominator;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    numerator = 2;
+    denominator = 3;
+
+    for (i = 1; i <= n; i++) {
+        sum += numerator / denominator;
+        numerator += 2;
+        denominator += 4;
+    }
+
+    printf("Sum of the series: %.2f\n", sum);
+    return 0;
+}
+// Write a program to print the following pattern:
+// *****
+// *****
+// *****
+// *****
+// *****
+#include <stdio.h>
+int main() {
+    int i, j;
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
